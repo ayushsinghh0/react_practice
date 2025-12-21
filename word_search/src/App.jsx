7201f061fdb7416e948aa708e383ee95086bf0c9
ayushsinghh0 <ayushraj4820@gmail.com>
2025-12-21 17:39:21 +0530

    specific word search added and colored them where word matches

--- word_search/src/App.jsx
+++ word_search/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       }
       index++;
     }
-    if(filterindex==filter.length)
+    if(filter.length > 0 && filterindex==filter.length)
       b.push(sentences[i]);
     else {
       c.push(sentences[i]);
@@ -53,13 +53,14 @@ function App() {
     }}></input>
     {
       b.map((sent,index)=>(
-        <p style = {{backgroundColor:"whitesmoke"}}key= {index} >{sent}</p>
+        <p style = {{backgroundColor:"whitesmoke"}}key= {index} >
+          <Color sentence={sent} filter={filter}></Color></p>
 
       ))
     }
     {
       c.map((sent,index)=>(
-        <p key= {index} >{sent}</p>
+        <p key= {index}  >{sent}</p>
 
       ))
     }
@@ -67,6 +68,44 @@ function App() {
   )
 }
 
+function Color({sentence,filter}){
+  let k=0;
+  let nonmatching=""
+  let endmatching=""
+  let matching=""
+  for(let i=0;i<sentence.length;i++){
+    if(k==0 && filter[k]!=sentence[i]){
+      nonmatching+=sentence[i];
+    }
+    if(sentence[i]==filter[k]&&k<filter.length){
+     k++;
+     matching+=sentence[i];
+    }
+    if(k>0 && k>=filter.length){
+      endmatching+=sentence[i];
+    }
+  }
+  return <>
+  <span>
+    {nonmatching}
+  </span>
+  <span style={{backgroundColor:"aqua"}}>
+    {matching}
+  </span>
+  <span>
+    {endmatching}
+  </span>
+  </>
+  //return 
+  // <span>
+  // <span>non matching value</span>
+  //<span>matching</span>
+  //<span>non matching value</span>
+  // </span>
+}
+
+
+/* 
 
 function Comp(){
   const [inputvalue,setInputvalue]=useState(1);
@@ -80,13 +119,13 @@ function Comp(){
   
   
   
-  return <div><input 
+  return <span><input 
   type="number" 
   onChange={(e)=>{
     setInputvalue(Number( e.target.value))
   }}></input><br></br>
   calculated :{b}
-  </div>
+  </span>
   
-}
+} */
 export default App
