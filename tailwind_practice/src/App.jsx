cc9176d639647bb03d0913afec9281392d2887bb
ayushsinghh0 <ayushraj4820@gmail.com>
2026-01-07 09:34:44 +0530

    practice  tailwind for youtube clone

--- tailwind_practice/src/App.jsx
+++ tailwind_practice/src/App.jsx
@@ -7,9 +7,12 @@ import { RevnueCard } from './components/Revenuecard'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  function ok(){
+    window.location="https://www.google.com/"
+  }
   return (
     <div className='grid grid-cols-4'>
+      <button onClick={ok}>okk</button>
     <RevnueCard title={"amount pending"} amount={"1000"} orderCount={2}/>
     </div>
   )
---
+++ youtube-clone/src/components/Videocard.tsx
@@ -0,0 +1,8 @@
+ export function VideoCard(){
+    return <div>
+       <img src="/photo.jpg"></img>
+       <div className="">
+        
+       </div>
+    </div>
+ }
