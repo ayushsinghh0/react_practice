feaf94c4be576d9323dc878f6abfdfd6971af9ca
ayushsinghh0 <ayushraj4820@gmail.com>
2026-01-09 23:38:42 +0530

    tailwind understand

--- youtube-clone/src/components/Videocard.tsx
+++ youtube-clone/src/components/Videocard.tsx
@@ -1,7 +1,15 @@
  export function VideoCard(){
     return <div>
        <img src="/photo.jpg"></img>
-       <div className="">
+       <div className="grid grid-cols-12  ">
+        <div className="col-span-1">
+            <img className="rounded-full w-20 h-20" src="/photo.jpg"></img>
+
+        </div>
+        <div className="col-span-10">
+            AYush: Level(Zero)
+            software loser engineer
+        </div>
         
        </div>
     </div>
